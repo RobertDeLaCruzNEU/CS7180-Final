69ad22f856a0a86f685e2a73cea1b3982e4b04ff
robertdelacruz1551 <snailinslowmotion@gmail.com>
2026-04-18 15:21:25 -0700

    added standardizing

--- models/preprocessors.py
+++ models/preprocessors.py
@@ -17,7 +17,9 @@ class BoundingBoxEngineering(BaseEstimator, TransformerMixin):
     def fit(self, X, y=None):
         return self
 
-    @staticmethod
+    def fit_transform(self, X, y = None, **fit_params):
+        return super().fit_transform(X, y, **fit_params)
+    
     def transform(self, X, y=None):
         """
         For each image, run the YOLO model to detect pedestrians and traffic 
@@ -26,19 +28,30 @@ class BoundingBoxEngineering(BaseEstimator, TransformerMixin):
         box coordinates and class label to the pixel values. Return a list 
         of frames, where each frame is a list of cropped objects.
 
-        X: A list of file paths to images.
-        y: Ignored.
+        Args:
+            X: A list of file paths to images.
+            y: Ignored.
+
+        Returns:
+            A list of frames (shape = (n, 10, 224, 224, 8)), where each frame is a 
+            list of cropped objects. Each cropped object is a 224x224x8 array, 
+            where the first 3 channels are the pixel values, and the last 5 
+            channels are the normalized bounding box coordinates and class label.
         """
         results = self.model(X)
         frames = []
         for result in results:
             image = result.orig_img[:, :, ::-1]
-            frame = []
+            W, H =image.shape[1], image.shape[0]
+            frame = np.zeros((10, 224, 224, 8), dtype=np.float32)
             xyxyl = result.boxes.data.cpu().numpy()
+            i = 0
             for x1, y1, x2, y2, _, cls in xyxyl:
+                if i >= 10:
+                    break
                 if cls not in [0, 9]:
                     continue
-                cls = (0 if cls == 0 else 1)
+                cls = (1 if cls == 0 else -1)
 
                 w, h = x2 - x1, y2 - y1
                 x, y = x1 + w / 2, y1 + h / 2
@@ -50,10 +63,10 @@ class BoundingBoxEngineering(BaseEstimator, TransformerMixin):
                 y2   = int(min(image.shape[0], y + side / 2))
 
                 crop = image[y1:y2, x1:x2]
-                crop = np.array(Image.fromarray(crop).resize((224, 224), Image.LANCZOS))
-
-                crop = np.concatenate([crop, np.tile([x, y, w, h, cls], (224, 224, 1))], axis=-1).astype(np.uint8)
-                frame.append(crop)
+                crop = np.array(Image.fromarray(crop).resize((224, 224), Image.LANCZOS)).astype(np.float32) / 255.0
+                meta = np.array([x, y, w, h, cls], dtype=np.float32) / np.array([W, H, W, H, 1], dtype=np.float32)
+                frame[i] = np.concatenate([crop, np.tile(meta, (224, 224, 1))], axis=-1)
+                i += 1
             frames.append(np.stack(frame, axis=0))
 
-        return frames
+        return np.array(frames)
