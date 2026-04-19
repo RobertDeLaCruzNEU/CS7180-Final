6b197a402ff92bc52c688a1cd3a2c7323a4b0af2
robertdelacruz1551 <snailinslowmotion@gmail.com>
2026-04-18 22:47:06 -0700

    save transformer

--- models/preprocessors.py
+++ models/preprocessors.py
@@ -9,13 +9,25 @@ from PIL import Image
 import pandas as pd
 import numpy as np
 import torch
+import gc
+import uuid
 
 class BoundingBoxEngineering(BaseEstimator, TransformerMixin):
-    def __init__(self, model = "yolo11n.pt", batch_size = 10, dsample = 128):
+    def __init__(self, model = "yolo11n.pt", batch_size = 10, dsample = 128, max_boxes = 10):
+        """
+        A custom transformer that uses a pre-trained YOLO model to detect pedestrians
+        and traffic lights in images, and transforms each image into a list of cropped objects with metadata.
+        Args:
+            model: The name of the pre-trained YOLO model to use (default: "yolo11n.pt").
+            batch_size: The number of images to process in a batch (default: 10).
+            dsample: The size to which each cropped object will be resized (default: 128).
+            max_boxes: The maximum number of objects to detect and crop from each image (default: 10).
+        """
         super().__init__()
         self.model = YOLO(model)
         self.batch_size = batch_size
         self.dsample = (dsample, dsample)
+        self.max_boxes = max_boxes
 
     def fit(self, X, y=None):
         return self
@@ -23,11 +35,11 @@ class BoundingBoxEngineering(BaseEstimator, TransformerMixin):
     def fit_transform(self, X, y = None, **fit_params):
         return super().fit_transform(X, y, **fit_params)
     
-    def transform(self, X, y=None):
+    def transform(self, X):
         """
         For each image, run the YOLO model to detect pedestrians and traffic 
         lights. For each detected object, crop the image to a square around 
-        the bounding box, resize it to 224x224, and concatenate the bounding 
+        the bounding box, resize it to 128x128, and concatenate the bounding 
         box coordinates and class label to the pixel values. Return a list 
         of frames, where each frame is a list of cropped objects.
 
@@ -36,41 +48,44 @@ class BoundingBoxEngineering(BaseEstimator, TransformerMixin):
             y: Ignored.
 
         Returns:
-            A list of frames (shape = (n, 10, 224, 224, 8)), where each frame is a 
-            list of cropped objects. Each cropped object is a 224x224x8 array, 
+            A list of frames (shape = (n, 10, 128, 128, 8)), where each frame is a 
+            list of cropped objects. Each cropped object is a 128x128x8 array, 
             where the first 3 channels are the pixel values, and the last 5 
             channels are the normalized bounding box coordinates and class label.
         """
-        frames = []
+        shape = (len(X), self.max_boxes, *self.dsample, 8)
+        memmap_name = str(uuid.uuid4())
+        frames = np.memmap(f'{memmap_name}.dat', dtype='float32', mode='w+', shape=shape)
+        # frames = np.zeros(shape, dtype=np.float32)
         
         for i in range(0, len(X), self.batch_size):
             batch_paths = X[i : i + self.batch_size]
             results = self.model(batch_paths, stream=True, verbose=False)
             
-            for result in results:
+            for idx, result in enumerate(results):
+                global_id = i + idx
                 image = result.orig_img[:, :, ::-1]
                 W, H = image.shape[1], image.shape[0]
-                frame = np.zeros((10, *self.dsample, 8), dtype=np.float32)
+                frame = np.zeros((self.max_boxes, *self.dsample, 8), dtype=np.float32)
                 
-                # Move boxes to CPU immediately to free GPU space
-                xyxyl = result.boxes.data.cpu().numpy()
+                boxes   = result.boxes.xywh.cpu().numpy()
+                classes = result.boxes.cls.cpu().numpy()
                 
-                count = 0
-                for x1, y1, x2, y2, _, cls in xyxyl:
-                    if count >= 10:
+                i = 0
+                for box, cls in zip(boxes, classes):
+                    if i >= self.max_boxes:
                         break
                     if cls not in [0, 9]:
                         continue
+                    x, y, w, h = box
                     
-                    cls_val = (1 if cls == 0 else -1)
-                    w, h = x2 - x1, y2 - y1
-                    cx, cy = x1 + w / 2, y1 + h / 2
+                    cls = (1 if cls == 0 else -1)
                     side = max(w, h)
 
-                    x1 = int(max(0, cx - side / 2))
-                    y1 = int(max(0, cy - side / 2))
-                    x2 = int(min(W, cx + side / 2))
-                    y2 = int(min(H, cy + side / 2))
+                    x1 = int(max(0, x - side / 2))
+                    y1 = int(max(0, y - side / 2))
+                    x2 = int(min(W, x + side / 2))
+                    y2 = int(min(H, y + side / 2))
 
                     crop = image[y1:y2, x1:x2]
                     
@@ -78,10 +93,14 @@ class BoundingBoxEngineering(BaseEstimator, TransformerMixin):
                         continue
                         
                     crop_resized = np.array(Image.fromarray(crop).resize(self.dsample, Image.LANCZOS)).astype(np.float32) / 255.0
-                    meta = np.array([cx, cy, w, h, cls_val], dtype=np.float32) / np.array([W, H, W, H, 1], dtype=np.float32)
+                    meta = np.array([x/W, y/H, w/W, h/H, cls/1], dtype=np.float32)
                     
-                    frame[count] = np.concatenate([crop_resized, np.tile(meta, (*self.dsample, 1))], axis=-1)
-                    count += 1
-                frames.append(frame)
-            torch.cuda.empty_cache()
-        return np.array(frames)
+                    frame[i] = np.concatenate([crop_resized, np.tile(meta, (*self.dsample, 1))], axis=-1)
+                    i += 1
+                frames[global_id] = frame
+
+            if i % 100 == 0:
+                gc.collect()
+                torch.cuda.empty_cache()
+
+        return frames
